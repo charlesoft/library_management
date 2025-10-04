32c5b5a5bd121319fdfdb03b45a1dc598741ef2e
Charles Furtado <charlees1509@hotmail.com>
2025-10-04 20:21:38 -0300

    Add authenticate methods to ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  include Devise::Controllers::Helpers
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -7,5 +8,32 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_role_id])
+  end
+
+  # Unified authentication supporting both controller specs (Devise sign_in)
+  # and request specs (JWT Authorization header)
+  def authenticate_user!
+    if defined?(warden) && warden&.user
+      return true
+    end
+
+    token = request.headers['Authorization']&.to_s
+    if token&.start_with?('Bearer ')
+      jwt = token.split(' ').last
+      begin
+        payload = Warden::JWTAuth::TokenDecoder.new.call(jwt)
+        @current_user = User.find_by(id: payload['sub'])
+        return true if @current_user
+      rescue StandardError
+        # fall through to unauthorized
+      end
+    end
+
+    render json: { message: 'Unauthorized' }, status: :unauthorized
+  end
+
+  def current_user
+    return warden.user if defined?(warden) && warden&.user
+    @current_user
   end
 end
